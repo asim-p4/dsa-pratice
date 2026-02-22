7bafdf07a9eb67b3db3506a0aa3bb3a1d868203b
AsimSilent <asim00silent99@gmail.com>
2026-02-22 13:19:58 +0500

    single dight, time O(n), space O(1)

---
+++ arrays/singleNumber.js
@@ -0,0 +1,20 @@
+// 136. Single Number
+
+// Difficulty: Easy
+// Platform: LeetCode
+
+// Frequently Asked In: Meta, Apple, Uber, Qualcomm, Airbnb, Zoho, Adobe, Amazon, Microsoft, Google
+
+//time O(n), space O(1)
+function singleDight(nums) {
+  let sum = 0;
+  for (let j = 0; j < nums.length; j++) {
+    sum = sum ^ nums[j];
+  }
+  return sum;
+}
+
+const array = [1, 2, 3, 4, 5, 5, 4, 1, 3];
+
+const res = singleDight(array);
+console.log(res);
