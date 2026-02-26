2f9ad284800739714b383aecf9373e16797e8a43
AsimSilent <asim00silent99@gmail.com>
2026-02-26 15:50:46 +0500

    sort colors using dutch national flag algorithm, time O(n), space O(1)

--- arrays/sortColors.js
+++ arrays/sortColors.js
@@ -15,5 +15,34 @@ function sort(nums) {
   }
 }
 
+
+//time O(n), space O(1)
+function dutchNationalFlagAlg(nums) {
+  let low = 0;                 // next position for 0
+  let mid = 0;                 // current index
+  let high = nums.length - 1;  // next position for 2
+
+  while (mid <= high) {
+
+    if (nums[mid] === 0) {
+      // Swap 0 to the front
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    }
+
+    else if (nums[mid] === 1) {
+      // 1 is already in correct middle region
+      mid++;
+    }
+
+    else { // nums[mid] === 2
+      // Swap 2 to the end
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+}
+
 const nums = [2, 0, 2, 1, 1, 0];
 sort(nums);
