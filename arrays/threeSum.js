045b809f9312cd42119f2ed74ee4023a88e1b0e4
AsimSilent <asim00silent99@gmail.com>
2026-02-27 23:20:49 +0500

    three sum, time(n^3), space(1), TLE

--- arrays/threeSum.js
+++ arrays/threeSum.js
@@ -10,7 +10,6 @@
 // time(n^3), space(1), TLE
 function threeSum(nums) {
   const ans = [];
-
   for (let i = 0; i < nums.length - 2; i++) {
     for (let j = i + 1; j < nums.length - 1; j++) {
       for (let k = j + 1; k < nums.length; k++) {
